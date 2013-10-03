b5f2332e1fbf4b50a8aaf504ba3b9d3da484f087
Matt Mowers <matt.mowers@gmail.com>
2013-10-02 23:58:37 -0700

    adding hover and active colors for menu items

--- sites/all/themes/lacuna/js/lacuna.js
+++ sites/all/themes/lacuna/js/lacuna.js
@@ -7,6 +7,16 @@
       $('ul.menu li a').click(function(e){
         e.stopProgagation();
       });
+      
+      //Adding classes to menu items for coloring. This is a hack, but I haven't yet thought
+      //of a better way within drupal to do this...
+      var color_array=["orange","light-blue","dark-blue","light-grey","green","purple"];
+      $("#block-menu-menu-top-right .content .menu li a").each(function(index){
+        $(this).addClass("lacuna-color-"+color_array[index%color_array.length]);
+      });
+      $("#block-menu-menu-left-menu .content .menu li a").each(function(index){
+        $(this).addClass("lacuna-color-"+color_array[index%color_array.length]);
+      });      
     }
   };
 }) (jQuery);
